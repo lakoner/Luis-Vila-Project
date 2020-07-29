d4edb517d973327cacf3b3cd3c8296d84024e207
Axel <axelcayuela@hotmail.com>
2020-07-29 09:42:54 +0200

    change/name_route inici to home

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/inici', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/obra','PageController@obra');
 
