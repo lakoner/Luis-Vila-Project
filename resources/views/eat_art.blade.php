ca4d4147c29dc75c4c5d4e7bb7bbacc042e931f7
Axel <axelcayuela@hotmail.com>
2020-08-16 21:27:42 +0200

    menu obras

--- resources/views/eat_art.blade.php
+++ resources/views/eat_art.blade.php
@@ -57,45 +57,40 @@
             </p>
         </div>
 <!--Carousel Wrapper-->
+<a href="#">
 <div id="multi-item-example" class="carousel slide carousel-multi-item carousel-multi-item-2" data-ride="carousel">
-
-  <!--Controls-->
-  <div class="controls-top">
-
-  </div>
-  <!--/.Controls-->
 
   <!--Slides-->
   <div class="carousel-inner" role="listbox">
- <a class="black-text1" href="#multi-item-example" data-slide="prev"><i class="fas fa-angle-left fa-3x pr-3"></i></a>
- <a class="black-text2" href="#multi-item-example" data-slide="next"><i class="fas fa-angle-right fa-3x pl-3"></i></a>
-    <!--First slide-->
+    <a class="black-text1" href="#multi-item-example" data-slide="prev"><i class="fas fa-angle-left fa-3x pr-3"></i></a>
+    <a class="black-text2" href="#multi-item-example" data-slide="next"><i class="fas fa-angle-right fa-3x pl-3"></i></a>
+        <!--First slide-->
     <div class="carousel-item active">
 
+     <div class="col-md-3 mb-3">
+       <div class="card">
+         <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_00.jpg" data-toggle="modal" data-target="#exampleModal1"
+            alt="Lluis Vila">
+        </div>
+      </div>
+
       <div class="col-md-3 mb-3">
         <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(38).jpg"
+          <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_01.jpg"
             alt="Card image cap">
         </div>
       </div>
 
       <div class="col-md-3 mb-3">
         <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(19).jpg"
+          <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_02.jpg"
             alt="Card image cap">
         </div>
       </div>
 
       <div class="col-md-3 mb-3">
         <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(42).jpg"
-            alt="Card image cap">
-        </div>
-      </div>
-
-      <div class="col-md-3 mb-3">
-        <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(8).jpg"
+          <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_03.jpg"
             alt="Card image cap">
         </div>
       </div>
@@ -108,28 +103,28 @@
 
       <div class="col-md-3 mb-3">
         <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(53).jpg"
+          <a href="#"><img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_04.jpg"
+            alt="Card image cap"></a>
+        </div>
+      </div>
+
+      <div class="col-md-3 mb-3">
+        <div class="card">
+          <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_06.jpg"
             alt="Card image cap">
         </div>
       </div>
 
       <div class="col-md-3 mb-3">
         <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(25).jpg"
+          <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_08.jpg"
             alt="Card image cap">
         </div>
       </div>
 
       <div class="col-md-3 mb-3">
         <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(47).jpg"
-            alt="Card image cap">
-        </div>
-      </div>
-
-      <div class="col-md-3 mb-3">
-        <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(26).jpg"
+          <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_010.jpg"
             alt="Card image cap">
         </div>
       </div>
@@ -142,28 +137,28 @@
 
       <div class="col-md-3 mb-3">
         <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(64).jpg"
+          <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_011.jpg"
             alt="Card image cap">
         </div>
       </div>
 
       <div class="col-md-3 mb-3">
         <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(51).jpg"
+          <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_014.jpg"
             alt="Card image cap">
         </div>
       </div>
 
       <div class="col-md-3 mb-3">
         <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(59).jpg"
+          <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_015.jpg"
             alt="Card image cap">
         </div>
       </div>
 
       <div class="col-md-3 mb-3">
         <div class="card">
-          <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(63).jpg"
+          <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_017.jpg"
             alt="Card image cap">
         </div>
       </div>
@@ -171,50 +166,52 @@
     </div>
     <!--/.Third slide-->
 
-  </div>
+  </div></a>
   <!--/.Slides-->
 
 </div>
-<!--/.Carousel Wrapper-->
+<div class="modal fade" id="exampleModal3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content1">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
 
-    <!-- <div class="slide_eat">
-        <div id="carouselExampleControls" class="carousel slide eat" data-ride="carousel">
-        <div class="carousel-inner">
-        <div class="carousel-item active">
-        <img src="/img/shoes/LluisVila_SabatesComestibles_00.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="/img/shoes/LluisVila_SabatesComestibles_01.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="/img/shoes/LluisVila_SabatesComestibles_02.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="/img/shoes/LluisVila_SabatesComestibles_03.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="/img/shoes/LluisVila_SabatesComestibles_04.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="/img/shoes/LluisVila_SabatesComestibles_05.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="/img/shoes/LluisVila_SabatesComestibles_06.jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-        <img src="/img/shoes/LluisVila_SabatesComestibles_07.jpg" class="d-block w-100" alt="...">
-        </div>
+    <div class="carousel-inner" role="listbox">
+
+    <a class="black-text1" href="#multi-item-example" data-slide="prev"><i class="fas fa-angle-left fa-3x pr-3"></i></a>
+    <a class="black-text2" href="#multi-item-example" data-slide="next"><i class="fas fa-angle-right fa-3x pl-3"></i></a>
+
+            <div class="carousel-item1">
+                <div class="col-md-3 mb-3">
+                    <div class="card">
+                        <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_00.jpg"
+                        alt="Card image cap">
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="col-md-3 mb-3">
+                    <div class="card">
+                        <img class="img-fluid" src="/img/shoes/mini/LluisVila_SabatesComestibles_00.jpg"
+                        alt="Card image cap">
+                    </div>
+                </div>
+            </div>
+
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tanca</button>
+      </div>
     </div>
-</div> -->
+  </div>
+</div>
+
 
 
 
--- resources/views/obra.blade.php
+++ resources/views/obra.blade.php
@@ -2,7 +2,19 @@
 
 
 @section('content')
+<main class="content-framwork-1">
+<div class="content-obra-menu">
+<div class="eat-art"><a href="#"><h3>EAT ART</h3></a></div>
+<div class="escultura"><a href="#"><h3>ESCULTURA</h3></a></div>
+<div class="volca"><a href="#"><h3>VOLCÀ</h3></a></div>
+<div class="pintura"><a href="#"><h3>PINTURA</h3></a></div>
+<div class="disseny"><a href="#"><h3>DISSENY</h3></a></div>
 
+
+
+
+</div>
+</main>
 @endsection
 
 
