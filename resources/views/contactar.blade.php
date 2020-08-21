c9126da2275b0341cc87868f3be0305c2b7c2f1b
Alejandro <alejandro9kgm@gmail.com>
2020-08-21 12:15:28 +0200

    Alejandro/ContactPg

--- resources/views/contactar.blade.php
+++ resources/views/contactar.blade.php
@@ -1,11 +1,38 @@
 @extends('templates.header_footer_front')
 
-
-
-
 @section('content')
 
-
+<body>
+    
+  
+      <div class="ImgFondo">
+        <div class="container">  
+          <form id="contact" action="" method="post">
+            <h3>Consulta</h3>
+            <h4>Para consultas generales puede contactar con nosotros rellenando el siguiente formulario:</h4>
+            <fieldset class="FielsetContact">
+              <input placeholder="Nombre" type="text" tabindex="1" required autofocus>
+            </fieldset class="FielsetContact">
+            <fieldset class="FielsetContact">
+              <input placeholder="Email Address" type="email" tabindex="2" required>
+            </fieldset>
+            <fieldset class="FielsetContact">
+              <input placeholder="Numero de Telefono (optional)" type="tel" tabindex="3" required>
+            </fieldset>
+            <fieldset class="FielsetContact">
+              <input placeholder="Nombre de Organizacion (optional)" type="text" tabindex="4" required>
+            </fieldset>
+            <fieldset class="FielsetContact">
+              <textarea placeholder="Mensaje" tabindex="5" required></textarea>
+            </fieldset>
+            <fieldset class="FielsetContact">
+              <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Enviar</button>
+            </fieldset>
+          </form>
+        </div>  
+      </div>
+    
+</body>
 @endsection
 
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,48 @@
 @extends('layouts.app')
 
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+<body>
+
+    <Header>
+        <div class="HeaderAdmin">
+            <div class="Referencia">
+                <div class="P-Referencia">
+                <p>Dashboard</p>
                 </div>
+                
+            </div>
+            <div class="Pagina"> <h1>Principal</h1></div>
+            <div class="Sesion">
+                <div class="dropdown">
+                
+            </div>
+        </div>
+    </Header>
+
+    <div class="Column">
+        <div class="row">
+            <div class="column" style="background-color:#FFFFFF;">
+              <h2>Paginas</h2>
+              <a href="enlacepagina.html">Obras</a>
+              <br>
+              <a href="enlacepagina.html">Editar obras</a>
+              <br>
+              <a href="enlacepagina.html">Subir nueva obra</a>
+              <br>
+              <a href="enlacepagina.html">noticias</a>
+              <br>
+              <a href="enlacepagina.html">Editar noticias</a>
+              <br>
+              <a href="enlacepagina.html">comments</a>
+              <br>
+              <h3>Crear nuevo registro</h3>
+              <br>
+              <a href="enlacepagina.html">table users</a>
             </div>
         </div>
     </div>
-</div>
+</body>
 @endsection
