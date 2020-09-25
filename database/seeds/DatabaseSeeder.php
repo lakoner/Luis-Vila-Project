67029a3fc5edb34cfe57c2c2ad0039b9c1c71954
Axel <axelcayuela@hotmail.com>
2020-09-25 18:36:20 +0200

    Factorys

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -46,11 +46,11 @@ class DatabaseSeeder extends Seeder
         factory(Serie::class)->create(['id' => 18, 'name' => 'Absolut','year'=>'2009-10']);
         factory(Serie::class)->create(['id' => 19, 'name' => '- sin serie -']);
 
-        // factory(Obra::class)->create(['id' => 1, 'name' => 'Nombre no especificado','image'=>asset('Luis-Vila-Project/storage/app/public/obres/Disseny/LluisVila_Paris.jpg'),
-        // 'year'=>'1985','technique'=>'- tecnica no especificada - ','categoria_id'=>3,'serie_id'=>19]);
+        factory(Obra::class)->create(['id' => 1, 'name' => 'Nombre no especificado','image'=>asset('Luis-Vila-Project/storage/app/public/obres/Disseny/LluisVila_Paris.jpg'),
+        'year'=>'1985','technique'=>'- tecnica no especificada - ','categoria_id'=>3,'serie_id'=>19]);
 
-        // factory(Actualidad::class)->create(['id' => 1, 'title' => 'Noticia1','image'=>'LluisVila__450.jpg','subtitle'=>'subtile1',
-        // 'text'=>'Hola que tal esto es el articulo o el texto de la noticia','date'=>'2013-04-13']);
+        factory(Actualidad::class)->create(['id' => 1, 'title' => 'Noticia1','image'=>'LluisVila__450.jpg','subtitle'=>'subtile1',
+        'text'=>'Hola que tal esto es el articulo o el texto de la noticia','date'=>'2013-04-13']);
 
         $this->call(CategoriaSeeder::class);
         $this->call(SerieSeeder::class);
